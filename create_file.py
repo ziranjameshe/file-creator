85105f1a68efa08c941bab55b7adf439f23ec0bb
Ziran <ziranjameshe@gmail.com>
2021-06-14 02:20:01 +0100

    add mode help

--- create_file.py
+++ create_file.py
@@ -10,7 +10,7 @@ parser.add_argument("file_type", choices = ['python', 'c', 'bash'], help = "Prov
 
 parser.add_argument("file_name", help = "Provide file name")
 
-parser.add_argument("-m", "--mode", choices = ['744', '644'], default = '744', help = "Provide file permission")
+parser.add_argument("-m", "--mode", choices = ['744', '644'], default = '744', help = "Provide file permission. This is mainly for python files.")
 
 args = parser.parse_args()
 
@@ -29,7 +29,9 @@ def write_python_file(file_name):
     '''Write python file'''
 
     file_content = "#!/usr/bin/env ipython3\n\n"
+
     file_content += "# Author : Ziran James He\n\n"
+
     file_content += "# Date   : %s\n\n" %datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
     with open(file_name, "w") as file:
@@ -42,8 +44,11 @@ def write_c_file(file_name):
     '''Write c file'''
 
     file_content = "/*\n"
+
     file_content += "Author : Ziran James He\n\n"
+
     file_content += "Date   : %s\n" %datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
     file_content += "*/\n"
 
     with open(file_name, "w") as file:
@@ -54,7 +59,9 @@ def write_bash_file(file_name):
     '''Write bash file'''
 
     file_content = "#!/usr/bin/bash\n\n"
+
     file_content += "# Author : Ziran James He\n\n"
+
     file_content += "# Date   : %s\n\n" %datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
     with open(file_name, "w") as file:
